7ea57c82bcca2cb0ec9fada716cef08cedead259
root <root@v178780.hosted-by-vdsina.ru>
2020-06-04 19:23:29 +0300

    Fixing Post Text

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import bridge from "@vkontakte/vk-bridge";
-import { Panel, PanelHeader, Button, Group, Div, Text } from '@vkontakte/vkui';
+import { Panel, PanelHeader, Button, Group, Div, Card, CardGrid, Text } from '@vkontakte/vkui';
 /*import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
@@ -16,12 +16,21 @@ const Home = ({ id, go, fetchedUser }) => (
 		<center><Text weight="semibold" style={{ marginBottom: 16, fontSize: 50, color: 'green' }}>Да</Text></center>
 		<Group title="Navigation Example">
 			<Div>
+			
 				<Button size="xl" level="2" onClick={() => bridge.send("VKWebAppJoinGroup", {"group_id": 196019037})}>
 					Вступить в группу с бесполезными скриптами
 				</Button><br />
-				<Button size="xl" level="2" onClick={() => bridge.send("VKWebAppShowWallPostBox", {"message": "Я опубликовал это с помощью VK Mini App!"})}>
+				<Group separator="hide" header={<Header mode="secondary">#СамыйГлупыйЧеллендж</Header>}>
+					<CardGrid>
+						<Card size="l" mode="shadow">
+							<center><Text weight="regular">Это приложение - из серии приложений «Senior Useless» (сеньор бесполезен). В данном приложении вы можете узнать, включено ли устройство, с которого вы посещаете наше приложение.</Text><br /><br /><Text weight="medium">Мы запускаем челлендж - опубликуйте на своей стене ВКонтакте предложенный ниже пост, чтобы сказать всем, что Ваше устройство включено, а также дайте возможность вашим друзьям проверить статус вашего устройства!</Text>
+				<Button mode="commerce" onClick={() => bridge.send("VKWebAppShowWallPostBox", {"message": "Благодаря приложению мне стало известно, что моё устройство включено!\n\nПопробуй и ты! Ссылка на проверку - https://vk.cc/av6cA2"})}>
 					Присоединиться к флешмобу
 				</Button>
+					</center>
+						</Card>
+					</CardGrid>
+				</Group>
 			</Div>
 		</Group>
 	</Panel>
