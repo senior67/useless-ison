2570399d8ac3fb52cb4e5367c7ae3d2236c3a5df
root <root@v178780.hosted-by-vdsina.ru>
2020-06-04 18:54:52 +0300

    Updates

---
+++ precache-manifest.d543f2e2d790d9fa0265bde5abf0128a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad55747af78926996feb7913f79a366",
+    "url": "/useless-ison/index.html"
+  },
+  {
+    "revision": "137021580cc0ebf17490",
+    "url": "/useless-ison/static/css/2.814df5ce.chunk.css"
+  },
+  {
+    "revision": "515a4883f3c6847cc9af",
+    "url": "/useless-ison/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "137021580cc0ebf17490",
+    "url": "/useless-ison/static/js/2.a327b906.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/useless-ison/static/js/2.a327b906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "515a4883f3c6847cc9af",
+    "url": "/useless-ison/static/js/main.c2af53f8.chunk.js"
+  },
+  {
+    "revision": "a5f10bf920981025a1d4",
+    "url": "/useless-ison/static/js/runtime-main.f05c5079.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/useless-ison/static/media/persik.4e1ec840.png"
+  }
+]);
