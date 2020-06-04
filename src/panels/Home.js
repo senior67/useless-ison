6601a5ef896d08f6a56d83ecbf9077290b2e0a14
root <root@v178780.hosted-by-vdsina.ru>
2020-06-04 20:49:39 +0300

    Adding photo and some minor fixes

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -14,22 +14,21 @@ const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
 		<PanelHeader>Включен ли девайс?</PanelHeader><br />
 		<center><Text weight="semibold" style={{ marginBottom: 16, fontSize: 50, color: 'green' }}>Да</Text></center>
-		<Group title="Navigation Example">
+		<Group title="#СамыйГлупыйЧеллендж">
 			<Div>
-			
-				<Button size="xl" level="2" onClick={() => bridge.send("VKWebAppJoinGroup", {"group_id": 196019037})}>
-					Вступить в группу с бесполезными скриптами
-				</Button><br />
 				<Group separator="hide" header={<Header mode="secondary">#СамыйГлупыйЧеллендж</Header>}>
 					<CardGrid>
 						<Card size="l" mode="shadow">
 							<br /><center><Text weight="regular">Это приложение - из серии приложений «Senior Useless» (сеньор бесполезен). В данном приложении вы можете узнать, включено ли устройство, с которого вы посещаете наше приложение.</Text><br /><br /><Text weight="medium">Мы запускаем челлендж - опубликуйте на своей стене ВКонтакте предложенный ниже пост, чтобы сказать всем, что Ваше устройство включено, а также дайте возможность вашим друзьям проверить статус вашего устройства!</Text><br />
-				<Button mode="commerce" onClick={() => bridge.send("VKWebAppShowWallPostBox", {"message": "Благодаря приложению мне стало известно, что моё устройство включено!\n\nПопробуй и ты! Ссылка на проверку - https://vk.cc/av6cA2"})}>
+				<Button mode="commerce" onClick={() => bridge.send("VKWebAppShowWallPostBox", {"message": "Благодаря приложению мне стало известно, что моё устройство включено!\n\nПопробуй и ты! Ссылка на проверку - https://vk.cc/av6cA2", "attachment": "photo-196019037_457239021"})}>
 					Присоединиться к флешмобу
 				</Button>
 					</center><br />
 						</Card>
-					</CardGrid>
+					</CardGrid><br />
+					<Button size="xl" level="2" onClick={() => bridge.send("VKWebAppJoinGroup", {"group_id": 196019037})}>
+					Вступить в группу с бесполезными скриптами
+				</Button>
 				</Group>
 			</Div>
 		</Group>
